932e81828e563b4b44a1e5658c45266f4d452f27
ashvin <ashvin.suthar777@gmail.com>
2016-06-20 00:38:41 +0100

    Commiting Missing Files

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,10 +1,3 @@
-$(document).ready(onDocumentReady);
-
-function onDocumentReady() {
-    var calendar = new Calendar();
-    var link = new Link();
-}
-
 function Link() {
     var $link = $(".link");
 
@@ -18,25 +11,85 @@ function Link() {
 
 }
 
-function Calendar() {
-    var $cal = $(".calendar");
-    var $calTitle = $cal.find(".center");
+function Calendar(param) {
+    var self = this;
+    var templateContainer = param.templateContainer || '<div class="calendar animated">' +
+        '<div class="calendar-inner">' +
+        '</div>' +
+        '</div>';
+
+    var templateHeader = param.templateHeader || '<div class="calendar-header">' +
+        '<div class="title"></div>' +
+        '<div class="calendar-header-inner">' +
+        '<div class="left link">' +
+        '<i class="icon-android-arrow-back"></i>' +
+        '</div>' +
+        '<div class="center"></div>' +
+        '<div class="right link">' +
+        '<i class="icon-android-arrow-forward"></i>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+
+    var templateContent = param.templateContent || '<div class="calendar-content">' +
+        '<div class="calendar-months">' +
+        '<div class="calendar-week-days">' +
+        '</div>' +
+        '<div class="calendar-months-inner">' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+
+    var $templateContainer = $(templateContainer);
+    var $templateHeader = $(templateHeader);
+    var $templateContent = $(templateContent);
+    var title = param.title || "";
+
+    $templateContainer.find(".calendar-inner").append($templateHeader);
+    $templateContainer.find(".calendar-inner").append($templateContent);
+
+    var $body = $("body");
+    var $el = $(param.el);
+    var $cal = $templateContainer;
+    var $calTitle = $cal.find(".title");
+    var $calTitleDate = $cal.find(".center");
     var $calPrevIcon = $cal.find(".left");
     var $calNextIcon = $cal.find(".right");
     var $calWeekDays = $cal.find(".calendar-week-days");
     var $calMonthsInner = $cal.find(".calendar-months-inner");
-    var $calMonth = $("<div class='calendar-month'></div>");
+    var $calMonth = $("<div class='calendar-month animated'></div>");
     var $calRow = $("<div class='calendar-row'></div>");
     var $calDay = $("<div class='calendar-day'><span></span></div>");
+    var $calWeekDay = $("<div class='calendar-week-day'></div>");
     var classOtherDay = "calendar-day-other";
     var classToday = "calendar-day-today";
-    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var classSelectedDay = "calendar-day-selected";
+    var monthNames = param.monthNames || ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var dayNamesShort = param.dayNamesShort || ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    var defaultDate = param.defaultDate || Date.today();
+    var dateFormat = param.dateFormat || 'M/d/yyyy';
+    var calendarDateFormat = param.calendarDateFormat || 'MMMM, yyyy';
+    var tagName = $el.prop('tagName').toLowerCase();
+    var isInput = tagName == "input";
+    var animationTime = 400;
+    var classAnimCalIn = "slideInUp";
+    var classAnimCalOut = "slideOutDown";
+    var classAnimationSlideOut = "slideOut";
+    var classAnimationSlideIn = "slideIn";
+    var $overlay = $("<div class='calendar-overlay overlay'></div>")
 
     var calendar = {
-        current: Date.today().moveToFirstDayOfMonth(),
+        current: new Date(defaultDate.toString()),
+        selectDate: new Date(defaultDate.toString()),
+        hammer : null,
         init: function() {
             calendar.bind();
             calendar.setMonth(false);
+            calendar.setDaysNames();
+            if (isInput) {
+                calendar.inputBinding();
+            }
+            calendar.setLabels();
         },
         bind: function() {
             $calNextIcon.on("click", {
@@ -45,6 +98,64 @@ function Calendar() {
             $calPrevIcon.on("click", {
                 direction: 'prev'
             }, calendar.goToMonth);
+            $cal.on('change', calendar.onDayChange);
+
+            calendar.hammer = new Hammer( $calMonthsInner[0] );
+
+            calendar.hammer.on("swipeleft", function(){
+              calendar.goToMonth({ data : { direction : "next"}})
+            });
+            calendar.hammer.on("swiperight", function(){
+              calendar.goToMonth({ data : { direction : "prev"}})
+            });
+        },
+        inputBinding: function() {
+            $el.val(calendar.selectDate.toString(dateFormat));
+            $el.on('click', calendar.onInputClick);
+            $body.append($templateContainer);
+            $body.append($overlay);
+        },
+        setLabels: function() {
+            $calTitle.html(title);
+        },
+        onInputClick: function() {
+            calendar.show();
+        },
+        setDaysNames: function() {
+            dayNamesShort.forEach(function(dayName) {
+                $calWeekDays.append($calWeekDay.clone().html(dayName));
+            });
+        },
+        onDayChange: function(event, date) {
+            if (isInput) {
+                $el.val(date);
+            }
+            $(self).trigger("change", date);
+            calendar.hide();
+        },
+        show: function() {
+            $templateContainer.show();
+            $overlay.show();
+            $templateContainer.addClass(classAnimCalIn);
+            $body.append
+            setTimeout(function() {
+                $templateContainer.removeClass(classAnimCalIn);
+            }, animationTime);
+        },
+        hide: function() {
+            $templateContainer.addClass(classAnimCalOut);
+            $overlay.hide();
+            setTimeout(function() {
+                $templateContainer.removeClass(classAnimCalOut);
+                $templateContainer.hide();
+            }, animationTime);
+        },
+        onDayClick: function(event) {
+            calendar.current = new Date(event.data.date);
+            calendar.selectDate = new Date(event.data.date);
+            $cal.trigger("change", calendar.selectDate.toString(dateFormat));
+            $("." + classSelectedDay).removeClass(classSelectedDay);
+            $(this).addClass(classSelectedDay);
         },
         goToMonth: function(event) {
             var currentMonth = calendar.current.getMonth();
@@ -57,7 +168,7 @@ function Calendar() {
             calendar.setMonth(true, event.data.direction);
         },
         setMonth: function(animate, direction) {
-            $calTitle.html(monthNames[calendar.current.getMonth()] + ", " + calendar.current.getFullYear());
+            $calTitleDate.html(calendar.current.toString(calendarDateFormat));
             calendar.setDaysOfMonth(calendar.current.moveToFirstDayOfMonth(), animate, direction); //curr month
         },
         setDaysOfMonth: function(monthFirstDate, animate, direction) {
@@ -92,16 +203,14 @@ function Calendar() {
         animateMonths: function($calMonthClone, direction) {
             var $currMonth = $calMonthsInner.find(".calendar-month");
             var $nextMonth = $calMonthClone;
-
-            $currMonth.addClass("calendar-month-off").addClass("animate-move-out-"+direction);
+            var outClass = direction == 'next' ? "Left" : "Right";
+            var inClass = direction == 'prev' ? "Left" : "Right";
+            $currMonth.addClass("calendar-month-off").addClass(classAnimationSlideOut + outClass);
             $calMonthsInner.append($nextMonth);
-            $nextMonth.addClass("animate-move-in-" + direction);
-
-            $currMonth.animate(300,
-                function() {
-                    $(this).remove();
-                }
-            );
+            $nextMonth.addClass(classAnimationSlideIn + inClass);
+            setTimeout(function() {
+                //$currMonth.remove();
+            }, animationTime);
         },
         addDataToDate: function($day, dayDate) {
             $day.attr("data-date", dayDate.toString());
@@ -110,9 +219,16 @@ function Calendar() {
             if (calendar.current.getMonth() != dayDate.getMonth()) {
                 $day.addClass(classOtherDay);
             }
-            if (Date.today().equals(dayDate)) {
+            if (today.equals(dayDate)) {
                 $day.addClass(classToday);
             }
+            if (calendar.selectDate.equals(dayDate)) {
+                $day.addClass(classSelectedDay);
+            }
+            $day.off('click', calendar.onDayClick);
+            $day.on('click', {
+                date: dayDate
+            }, calendar.onDayClick);
         }
     };
     calendar.init();
